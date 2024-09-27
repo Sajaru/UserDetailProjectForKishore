2dd9be524fadb3cad5ae0fe12448edcd1da9aabc
pulakkundu <pulakundu@gmail.com>
2024-09-28 00:42:05 +0530

    formatting

--- src/main/java/com/Pulak/userDOB/UserDobApplication.java
+++ src/main/java/com/Pulak/userDOB/UserDobApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserDobApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserDobApplication.class, args);
+	public static void main( String[] args) {
+		SpringApplication.run( UserDobApplication.class, args );
 	}
 
 }
